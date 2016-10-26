b410f5131f8a022f6e1929dc9a1b31304ac83e7a
Joe Jordan <joe.jordan@outlook.com>
2016-10-26 14:52:58 -0400

    push script

--- push.sh
+++ push.sh
@@ -7,12 +7,13 @@
 
 
 #   git checkout -b gh-pages
+git checkout master -b
   git add dist/ *
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 
 
-  git remote add origin-pages https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git # >  /dev/null 2>&1
   git push origin master
 #   git push --quiet --set-upstream origin-pages gh-pages 
 
