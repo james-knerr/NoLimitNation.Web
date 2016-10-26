be51d7619be52bf1032f31a62e860107968f04b7
Joe Jordan <joe.jordan@outlook.com>
2016-10-26 14:58:25 -0400

    push script

--- push.sh
+++ push.sh
@@ -1,20 +1,28 @@
-#!/bin/sh
+    #!/bin/sh
+
+    echo "~~~ Setting User Info"
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
 
 
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+    echo "~~~ Checking Master Out"
+    #   git checkout -b gh-pages
+    git checkout master -b
+
+    echo "~~~ Adding dist content"
+    git add dist/ *
+
+    echo "~~~ Committing dist content"
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 
+    echo "~~~ Adding remote originn https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git"
+    git remote add origin https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git # >  /dev/null 2>&1
 
-#   git checkout -b gh-pages
-git checkout master -b
-  git add dist/ *
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    echo "~~~ Pushing Master to origin"
+    git push origin master
 
-
-
-  git remote add origin https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git # >  /dev/null 2>&1
-  git push origin master
-#   git push --quiet --set-upstream origin-pages gh-pages 
+    echo "~~~ Complete ~~~"
+    #   git push --quiet --set-upstream origin-pages gh-pages 
 
 
