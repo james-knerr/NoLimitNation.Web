3c6d6138420d03115daac30af600defc39d57e5e
Joe Jordan <joe.jordan@outlook.com>
2016-10-26 19:26:42 -0400

    push script

--- push.sh
+++ push.sh
@@ -1,32 +1,31 @@
     #!/bin/sh
 
-    # echo "~~~ Setting User Info"
-    # git config --global user.email "joe.jordan@outlook.com"
-    # git config --global user.name "conFusem3"
+    echo "~~~ Setting User Info"
+    git config --global user.email "joe.jordan@outlook.com"
+    git config --global user.name "conFusem3"
 
 
-    # echo "~~~ Checking Master Out"
-    # #   git checkout -b gh-pages
-    # git checkout master -b
+    echo "~~~ Checking Master Out"
+    #   git checkout -b gh-pages
+    git checkout master -b
 
-    # echo "~~~ Adding dist content"
-    # git add .
+    echo "~~~ Adding dist content"
+    git add .
 
-    # echo "~~~ Committing dist content"
-    # git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    echo "~~~ Committing dist content"
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 
-    # echo "~~~ Adding remote originn https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git"
-    # git remote add origin https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git # >  /dev/null 2>&1
+    echo "~~~ Adding remote originn https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git"
+    git remote add origin https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git # >  /dev/null 2>&1
 
-    # echo "~~~ Pushing Master to origin"
-    # git push origin master
+    echo "~~~ Pushing Master to origin"
+    git push origin master
 
-    # echo "~~~ Complete ~~~"
-    # #   git push --quiet --set-upstream origin-pages gh-pages 
+    echo "~~~ Complete ~~~"
+    #   git push --quiet --set-upstream origin-pages gh-pages 
 
-set -ev
 
-$ git push https://github.com/NoLimitNation/Web.Build.git
-Username: djordan25
-Password: ${GH_TOKEN}
+# $ git push https://github.com/NoLimitNation/Web.Build.git
+# Username: djordan25
+# Password: ${GH_TOKEN}
