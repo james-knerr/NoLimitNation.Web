7250d2343fb0e490fbe230ef7618cfb7fc97c6bc
Joe Jordan <joe.jordan@outlook.com>
2016-10-26 14:45:13 -0400

    push script

--- push.sh
+++ push.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-setup_git() {
+
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-}
 
-commit_website_files() {
+
+
 #   git checkout -b gh-pages
   git add dist/ *
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
 
-upload_files() {
+
+
   git remote add origin-pages https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git > /dev/null 2>&1
   git push origin master
 #   git push --quiet --set-upstream origin-pages gh-pages 
-}
 
-setup_git
-commit_website_files
-upload_files
+
