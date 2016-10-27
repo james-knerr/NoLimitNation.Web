99c76260ef6c7458be58ee78e8d909e7ec217bba
Joe Jordan <joe.jordan@outlook.com>
2016-10-26 20:05:18 -0400

    push script

--- push.sh
+++ push.sh
@@ -1,28 +1,18 @@
     #!/bin/sh
     set -ve
-    echo "~~~ Setting User Info"
     git config --global user.email "joe.jordan@outlook.com"
     git config --global user.name "conFusem3"
 
 
-    echo "~~~ Checking Master Out"
     #   git checkout -b gh-pages
-    git checkout master -b
-
-    echo "~~~ Adding dist content"
+    git checkout -b master master
     git add .
-
-    echo "~~~ Committing dist content"
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-
-    echo "~~~ Adding remote originn https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git"
     git remote add origin https://${GH_TOKEN}@github.com/NoLimitNation/Web.Build.git # >  /dev/null 2>&1
 
-    echo "~~~ Pushing Master to origin"
     git push origin master
 
-    echo "~~~ Complete ~~~"
     #   git push --quiet --set-upstream origin-pages gh-pages 
 
 
